2f79bd536da29aca2835432a32bae337cf3cb67e
MauricioGedrat <mauriciogedrat@gmail.com>
2021-12-01 12:53:33 -0300

    git

---
+++ components/common/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from './Logo'
+
+const Header = () => {
+    return (
+        <div className='bg-header py-8'>
+            <div className='ml-32'>
+                <Logo />
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
