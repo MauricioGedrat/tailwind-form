273f1b1da434a005f4e065673a5c7d288646bea4
Jonas Gedrat <jonas.gedrat@fullgauge.com.br>
2021-12-01 14:58:50 -0300

    hover scale

--- components/common/Header.jsx
+++ components/common/Header.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import Logo from './Logo'
+import React from "react";
+import Logo from "./Logo";
 
 const Header = () => {
-    return (
-        <div className='bg-header py-8'>
-            <div className='ml-32'>
-                <Logo />
-            </div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="bg-header py-8">
+      <div className="ml-32">
+        <Logo />
+      </div>
 
-export default Header
+      <span className="text-gray-800 border-transparent border-b-2 hover:border-gray-600 hover:text-gray-800 px-3 py-2 text-sm font-medium uppercase">
+        Home
+      </span>
+    </div>
+  );
+};
+
+export default Header;
